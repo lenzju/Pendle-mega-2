5413c4defb5062087d6901b7ad4d61eeec04037a
lenzju <lenzju@katharineum.de>
2026-04-28 15:15:12 +0200

    Update physics.py

--- utils/physics.py
+++ utils/physics.py
@@ -1,26 +1,25 @@
 import numpy as np
 from scipy.signal import find_peaks
 
-vx = np.gradient(x, times)
-vy = np.gradient(y, times)
-
-v = np.sqrt(vx**2 + vy**2)
 def analyze_motion(times, positions, length):
-    # Peaks finden (Maxima der Bewegung)
-    peaks, _ = find_peaks(positions)
+    positions = np.array(positions)
+
+    x = positions[:, 0]
+    y = positions[:, 1]
+
+    # Peaks über x (stabil)
+    peaks, _ = find_peaks(x)
 
     peak_times = times[peaks]
 
-    # Periodendauer berechnen
-    periods = np.diff(peak_times)
-
-    if len(periods) == 0:
+    if len(peak_times) < 2:
         return 0, 0, 0
+
+    periods = np.diff(peak_times)
 
     T = np.mean(periods)
     f = 1 / T
 
-    # g berechnen
     g = (4 * np.pi**2 * length) / (T**2)
 
     return T, f, g
